b093b2e220723a312016304cbe6ec62eaa49fdb6
lusorich <dreamonautre@gmail.com>
2020-11-04 18:58:42 +0500

    build

---
+++ js/script.js
@@ -0,0 +1,20 @@
+  var navMain = document.querySelector('.main-nav');
+  var navToggle = document.querySelector('.main-nav__toggle');
+
+  navMain.classList.remove('main-nav--nojs');
+
+  navToggle.addEventListener('click', function() {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    navMain.classList.remove("main-nav--opened");
+    navMain.classList.remove("main-nav--nojs");
+    navMain.classList.add("main-nav--closed");
+  });
